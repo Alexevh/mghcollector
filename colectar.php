ad858458e4006d9d092b4a2ce8cd3ead724d67cf
alex <alex@Varian>
2019-01-29 10:05:59 -0300

    Se corrije cabezal

--- colectar.php
+++ colectar.php
@@ -1,23 +1,25 @@
 <?php
 
+//header("Access-Control-Allow-Origin: *");
+//header("Access-Control-Allow-Methods: GET");
+//header("Access-Control-Allow-Headers: Content-Type");
+//header("Content-Type: application/json; charset=UTF-8");
+
 require_once ("bd/conexion.php");
+
 
 $nombre = $_GET["usuario"];
 $pass = $_GET["pass"];
 $nombre = stripslashes($nombre);
 $pass = stripslashes($pass);
 
-
 $pagina = $_GET["pagina"];
 $fecha = $_GET["fecha"];
 $tiempo = $_GET["tiempo"];
 
-
-
 $conexion = new Conexion();
-
-
 $datos = new Datos();
+
 
 if ($datos->login($nombre, $pass)) {
     try {
